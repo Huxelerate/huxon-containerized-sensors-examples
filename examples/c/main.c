d24abc950f94e4b77721a990f269e0b12df7a102
Alessandro Comodi <alessandro.comodi@huxelerate.it>
2022-12-12 18:30:47 +0100

    examples: c: add huxelerate copyright message

--- examples/c/main.c
+++ examples/c/main.c
@@ -1,4 +1,5 @@
 // Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Huxelerate.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stdio.h>
